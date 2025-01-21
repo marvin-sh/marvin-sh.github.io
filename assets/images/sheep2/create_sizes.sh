bef0c6f0362f2b0b1e0bc85bb9242f4171714315
Marvin <marvin-krauspenhaar@uni-a.de>
2025-01-22 02:25:51 +1300

    Edit css & Added banner
    * increased font size
    * added sheep banner
    * changed heading color
    * changed inner max width (width of text body)
    * changed padding

---
+++ assets/images/sheep2/create_sizes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+convert sheep2_distance.png -resize 960x120 sheep2_distance_120.png
+convert sheep2_distance.png -resize 640x80 sheep2_distance_80.png
+convert sheep2_distance.png -resize 320x40 sheep2_distance_40.png
+
